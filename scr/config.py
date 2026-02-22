5677ab706b948d85c3140734091d262612b5df4b
OUZZINE <souzzine9@gmail.com>
2026-02-22 15:10:51 +0100

    Create config.py

---
+++ scr/config.py
@@ -0,0 +1,10 @@
+# Paramètres SQL
+# Paramètres de connexion à la base SQL contenant la table "recidive"
+
+DB_CONFIG = {
+    "host": "localhost",
+    "port": 5432,
+    "user": "said",
+    "password": "password",
+    "database": "criminologie"
+}
